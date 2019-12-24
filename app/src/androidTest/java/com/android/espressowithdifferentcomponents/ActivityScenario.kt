b2d867650f08124dfc27c3092762e5e0c819623f
Danielius <dnlspasin@gmail.com>
2019-12-24 17:37:25 +0200

    Activity scenario test

---
+++ app/src/androidTest/java/com/android/espressowithdifferentcomponents/ActivityScenario.kt
@@ -0,0 +1,37 @@
+package com.android.espressowithdifferentcomponents
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Before
+import org.junit.Test
+import org.junit.internal.runners.JUnit4ClassRunner
+import org.junit.runner.RunWith
+import java.util.regex.Pattern.matches
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class ActivityScenario {
+
+
+    @Before
+    fun launchActivity() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun activityIsolationTest() {
+        onView(withId(R.id.firstLayout)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.navigateButton)).perform(ViewActions.click())
+
+//        onView(withId(R.id.secondLayout)).check(ViewAssertions.matches(isDisplayed()))
+
+        //Another way to check visibility
+
+        onView(withId(R.id.secondLayout)).check(ViewAssertions.matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.secondTextView)).check(ViewAssertions.matches(withText("WELCOME TO SECOND ACTIVTY!")))
+    }
+
+}
--- app/src/main/java/com/android/espressowithdifferentcomponents/MainActivity.kt
+++ app/src/main/java/com/android/espressowithdifferentcomponents/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.android.espressowithdifferentcomponents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        button = findViewById(R.id.navigateButton)
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
